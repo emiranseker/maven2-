ddc28176b7943220a286f3e2a1abac31041bce54
emiranseker <emirhan.seker@std.medipol.edu.tr>
2024-01-12 12:08:30 +0300

    maven

---
+++ src/test/resources/tr/edu/medipol/yazilim/hesaplamalarTest.java
@@ -0,0 +1,34 @@
+package tr.edu.medipol.yazilim;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class hesaplamalarTest {
+
+	@Test
+	void testFaktoriyelrec() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFaktoriyelloop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTopla() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCikart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCarpma() {
+		fail("Not yet implemented");
+	}
+
+}
